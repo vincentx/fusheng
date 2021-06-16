a97cecede7f4e1f0e9907e6f4923b659c4fa58e1
yedili <yedili@thoughtworks.com>
2021-06-16 20:48:54 +0800

    [server][#2][mingman&tianpeng&yaer] update tests

--- plugin/src/functionalTest/java/fusheng/FushengPluginFunctionalTest.java
+++ plugin/src/functionalTest/java/fusheng/FushengPluginFunctionalTest.java
@@ -1,18 +1,22 @@
 package fusheng;
 
+import static org.gradle.internal.impldep.org.hamcrest.CoreMatchers.equalTo;
+import static org.gradle.internal.impldep.org.hamcrest.CoreMatchers.is;
+import static org.gradle.internal.impldep.org.hamcrest.MatcherAssert.assertThat;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Writer;
 import java.nio.file.Files;
-import org.junit.Test;
-
 import org.gradle.testkit.runner.BuildResult;
 import org.gradle.testkit.runner.GradleRunner;
+import org.junit.Test;
 
 public class FushengPluginFunctionalTest {
     @Test
     public void canRunTask() throws IOException {
+
         File projectDir = new File("build/functionalTest");
         Files.createDirectories(projectDir.toPath());
         writeString(new File(projectDir, "settings.gradle"), "");
@@ -29,9 +33,9 @@ public class FushengPluginFunctionalTest {
         runner.withProjectDir(projectDir);
         BuildResult result = runner.build();
 
-        // Verify the result
-        System.out.println(result.getOutput());
-//        assertTrue(result.getOutput().contains("Hello from plugin 'fusheng.startLivingDoc'"));
+        final var startLivingDocTask = result.getTasks().get(0);
+        assertThat(startLivingDocTask.getPath(), is(equalTo(":startLivingDoc")));
+        assertThat(startLivingDocTask.getOutcome(), is(equalTo("SUCCESS")));
     }
 
     private void writeString(File file, String string) throws IOException {
