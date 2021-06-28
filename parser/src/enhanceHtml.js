26a89f435690fc2d4e54df8263c7dc548a572964
Zijie Jiang <zijie.jiang@thoughtworks.com>
2021-06-28 22:55:11 +0800

    modify id to uuid

--- parser/src/enhanceHtml.js
+++ parser/src/enhanceHtml.js
@@ -1,25 +1,26 @@
 import { CODE_TAG, CODE_TAG_SELECTOR, EXAMPLE_ID, ASSERTION_ID, EMPTY } from './constant'
 import cheerio from 'cheerio'
 import {addStyle} from './style'
+import { v4 as uuidv4 } from 'uuid'
 
 export default (html) => {
     const $ = cheerio.load(html)
     $(CODE_TAG_SELECTOR.EXAMPLE).each(function (exampleIndex, example) {
-        enhance($(example), exampleIndex, CODE_TAG.EXAMPLE)
+        enhance($(example), CODE_TAG.EXAMPLE)
         $(example).find(CODE_TAG_SELECTOR.ASSERTION).each(function (assertionIndex, assertion) {
-            enhance($(assertion), assertionIndex, CODE_TAG.ASSERTION)
+            enhance($(assertion), CODE_TAG.ASSERTION)
         })
     })
     addStyle($)
     return $.html()
 }
 
-const enhance = (el, uuid, type) => {
+const enhance = (el, type) => {
     if (type === CODE_TAG.EXAMPLE) {
-        el.attr(EXAMPLE_ID, uuid.toString())
+        el.attr(EXAMPLE_ID, uuidv4())
     }
     if (type === CODE_TAG.ASSERTION) {
-        el.attr(ASSERTION_ID, uuid.toString())
+        el.attr(ASSERTION_ID, uuidv4())
         const text = el.text()
         el.text(EMPTY)
         el.append(`<span class="assert-expect">${text}</span>`)
