e610edf617833e6f822b3080d5f5f3bd185663c5
Ruiling Guo <ruiling.guo@thoughtworks.com>
2021-07-05 23:10:07 +0800

    [parser][tech][lyliu&zijie&ruiling&luoxiao] fix converter

--- parser/src/ast-converter.js
+++ parser/src/ast-converter.js
@@ -2,7 +2,7 @@ import converter from 'html-parse-stringify'
 
 export default {
   toDomJSON(html) {
-    let str = html.trim().replace(/\s+/g, "")
+    let str = html.trim().replace(/\s{2,}/g, "")
     return converter.parse(str)
   },
 
