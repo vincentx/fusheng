2b33c085df5b5491bc3b964ea9dc0dd7468bcd79
Zijie Jiang <zijie.jiang@thoughtworks.com>
2021-07-13 10:03:08 +0800

    [parser] fix index test

--- parser/test/index.test.js
+++ parser/test/index.test.js
@@ -3,21 +3,32 @@ import {load} from 'cheerio'
 import {v4} from 'uuid'
 import {exec} from '../src'
 
-test('index.js', () => {
+test('test generated script', () => {
   // init $, fixture, context
   const html = readFileSync('public/example.html', 'utf-8')
   const $ = load(html)
   const script = exec($, v4)
-  const context = {
-    result: {}
-  }
+  const context = {}
   const fixture = defaultFixture()
   let actualPlayers
+  let actualMinWager
+  let actualRaiseWager
   fixture.newGame = function(players) {
     actualPlayers = players
   }
-  execute(script, $, context, fixture)
-  expect(actualPlayers).toBe('A,B,C')
+  fixture.setMinWager = function (minWager) {
+    actualMinWager = minWager
+  }
+  fixture.raise = function (wager) {
+    actualRaiseWager = wager
+  }
+  $('.example').each((index, element) => {
+    const uuid = $(element).attr('id')
+    execute(script[uuid], $, context, fixture)
+    expect(actualPlayers).toBe('A,B,C')
+    expect(actualMinWager).toBe(3)
+    expect(actualRaiseWager).toBe(5)
+  })
 })
 
 function execute(script, $, context, fixture) {
