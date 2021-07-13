6fe642b144b1b8598a75279f126d5db142c3ef23
lyliu <lyliu@thoughtworks.com>
2021-07-13 10:46:20 +0800

    [parser] add test 3-depth embedded code for parse.js

--- parser/test/parse.test.js
+++ parser/test/parse.test.js
@@ -23,7 +23,7 @@ test('parse html sequentially', () => {
   expect(codes).toBe(`variableCode;functionCode(){};assertionCode(){};`)
 })
 
-test('parse html depth-first', () => {
+test('parse 2-depth html depth-first', () => {
   const $ = load(`
     <p class="example">
       <span>
@@ -49,4 +49,37 @@ test('parse html depth-first', () => {
 
   const codes = parse($, root, 'id', parseUtils)
   expect(codes).toBe(`variableCode;functionCode(){variableCode;};assertionCode(){};`)
+})
+
+test('parse 3-depth html depth-first', () => {
+  const $ = load(`
+    <p class="example">
+      <span>
+        现在有玩家
+        <span class="variable" data-name="players">A,B,C</span>
+        参与游戏，
+      </span>
+      <span class="function" data-action="setMinWager" data-params="minWager">
+        <span>
+          最小下注金额为<span class="variable" data-name="minWager">3</span>，
+          <span class="function" data-action="setWager" data-params="wager">
+            玩家A的筹码数为<span class="variable" data-name="wager">100</span>
+          </span>,
+        </span>
+      </span>
+      <span>
+        底池金额为
+        <span class="assertion" data-expect="equal" data-action="getPot">0</span>。
+      </span>   
+    </p>
+  `)
+  const root = $('.example')
+  const parseUtils = {
+    parseVariable: (node, codes) => { codes.push(`variableCode;`) },
+    parseFunction: (node, codes, embeddedCode) => { codes.push(`functionCode(){${embeddedCode}};`) },
+    parseAssertion: (node, id, codes, embeddedCode) => { codes.push(`assertionCode(){${embeddedCode}};`) },
+  }
+
+  const codes = parse($, root, 'id', parseUtils)
+  expect(codes).toBe(`variableCode;functionCode(){variableCode;functionCode(){variableCode;};};assertionCode(){};`)
 })
