792081ca4c22c6bdbbc9b52f495a8f4e4b8a4c52
Jiahao Li <lijiahao_shandong@163.com>
2021-07-21 20:45:52 +0800

    [runner][huangcheng] modify package name

--- runner/lib/src/main/java/com/thoughtworks/fusheng/adapter/ServerAdapter.java
+++ runner/lib/src/main/java/com/thoughtworks/fusheng/adapter/ServerAdapter.java
@@ -6,8 +6,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ServerAdapter {
-    public static String runExperiment(String specName, String htmlContent) throws ClassNotFoundException {
-        Class<?> spec = Class.forName("com.thoughtworks.fusheng.fixture." + specName);
+
+    // fullSpecName 中包含完整的包名，例如：com.thoughtworks.fusheng.fixture.TestSpec
+    public static String runExperiment(String fullSpecName, String htmlContent) throws ClassNotFoundException {
+        Class<?> spec = Class.forName(fullSpecName);
         RunnerFacadeImpl runner = new RunnerFacadeImpl(spec, htmlContent);
 
         runner.getRunnerResource().getExampleResources().forEach(exampleResource -> {
@@ -15,7 +17,7 @@ public class ServerAdapter {
             runner.run(exampleName);
         });
 
-        String experimentSpecName = ServerAdapter.getNewExperimentSpecName(specName);
+        String experimentSpecName = ServerAdapter.getNewExperimentSpecName(fullSpecName);
         Path reportFilePath = ServerAdapter.getExperimentReportFilePath(experimentSpecName);
 
         runner.saveDomJSONToFile(reportFilePath);
