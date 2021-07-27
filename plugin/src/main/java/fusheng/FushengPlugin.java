59366d52a112c26f2d530dc024f24bb87758d7e5
mingman.zheng <mingman.zheng@thoughtworks.com>
2021-07-27 08:33:05 +0800

    [server][mingman] solve package issue and be closable

--- plugin/src/main/java/fusheng/FushengPlugin.java
+++ plugin/src/main/java/fusheng/FushengPlugin.java
@@ -1,25 +1,24 @@
 package fusheng;
 
-import org.apache.log4j.Logger;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
 public class FushengPlugin implements Plugin<Project> {
 
     public void apply(Project project) {
-        final var server = new FushengServer();
-        project.getTasks().register("startLivingDoc", task -> task.doLast(s -> runServer(server)));
-        project.getTasks().register("stopLivingDoc", task -> task.doLast(s -> server.stopServer()));
-        project.getTasks().register("fusheng", task -> task.dependsOn("startLivingDoc").finalizedBy("stopLivingDoc"));
+        project.getTasks().register("startLivingDoc", task -> task.doLast(s -> runServer()));
     }
 
-    private void runServer(final FushengServer server) {
+    private void runServer() {
+        final var server = new FushengServer();
         try {
             server.startServer();
             Thread.sleep( 60000 );
         }
         catch(Exception e) {
             e.printStackTrace();
+        }finally {
+            server.stopServer();
         }
     }
 }
