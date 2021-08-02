b83de47cf7851df9a741ad5c7c21cdd6e9cd0c3e
mingman.zheng <mingman.zheng@thoughtworks.com>
2021-08-02 20:37:19 +0800

    [server][yaer && mingman && tp] update config

--- plugin/src/main/java/com/thoughtworks/fusheng/config/ServerConfig.java
+++ plugin/src/main/java/com/thoughtworks/fusheng/config/ServerConfig.java
@@ -6,9 +6,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class ServerConfig {
-    // TODO: read configuration
-    private String rawSpecFolderLocation = "/src/main/resources/specs";
-    private String reportFolderLocation = "/src/main/resources/reports";
+    private String rawSpecFolderLocation = "src/test/resources/fusheng/spec";
+    private String reportFolderLocation = "/build/reports/tests/fusheng/result";
     private String experimentFolderLocation = "/build/reports/tests/fusheng/experiment";
     private String indexHtmlLocation = "/src/main/resources/index/fusheng-combined.html";
 }
