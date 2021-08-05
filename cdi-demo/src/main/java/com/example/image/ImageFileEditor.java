743922394c2793a54e6a34c472f2b32098cce773
Zijie Jiang <zijie.jiang@thoughtworks.com>
2021-08-05 21:19:24 +0800

    remove cdi demo

--- cdi-demo/src/main/java/com/example/image/ImageFileEditor.java
+++
@@ -1,5 +0,0 @@
-package com.example.image;
-
-public interface ImageFileEditor {
-    String openFile(String fileName);
-}
--- cdi-demo/src/main/java/com/example/image/PngFileEditor.java
+++
@@ -1,9 +0,0 @@
-package com.example.image;
-
-@PngQualifier
-public class PngFileEditor implements ImageFileEditor {
-    @Override
-    public String openFile(String fileName) {
-        return "Opening PNG file " + fileName;
-    }
-}
